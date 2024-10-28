e1b0a2b93baaf4ba8b375befb8410d3f6aa82a32
davidelng <davidelngyt@gmail.com>
2024-10-28 23:05:07 +0100

    more cli args

--- configure.go
+++ configure.go
@@ -9,9 +9,16 @@ import (
 type config struct {
 	pages              map[string]int
 	baseURL            *url.URL
+	maxPages           int
 	mu                 *sync.Mutex
 	concurrencyControl chan struct{}
 	wg                 *sync.WaitGroup
+}
+
+func (cfg *config) pagesLen() int {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+	return len(cfg.pages)
 }
 
 func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) {
@@ -27,7 +34,7 @@ func (cfg *config) addPageVisit(normalizedURL string) (isFirst bool) {
 	return true
 }
 
-func configure(rawBaseURL string, maxConcurrency int) (*config, error) {
+func configure(rawBaseURL string, maxConcurrency, maxPages int) (*config, error) {
 	baseURL, err := url.Parse(rawBaseURL)
 	if err != nil {
 		return nil, fmt.Errorf("couldn't parse base URL: %v", err)
@@ -36,6 +43,7 @@ func configure(rawBaseURL string, maxConcurrency int) (*config, error) {
 	return &config{
 		pages:              make(map[string]int),
 		baseURL:            baseURL,
+		maxPages:           maxPages,
 		mu:                 &sync.Mutex{},
 		concurrencyControl: make(chan struct{}, maxConcurrency),
 		wg:                 &sync.WaitGroup{},
